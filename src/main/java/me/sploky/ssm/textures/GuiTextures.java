6d5ad4844abd8586bea2b3499c0e2483d6dad6b0
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-05-21 21:37:24 -0400

    Added GuiTextures class

---
+++ src/main/java/me/sploky/ssm/textures/GuiTextures.java
@@ -0,0 +1,8 @@
+package me.sploky.ssm.textures;
+
+import me.sploky.ssm.SplokysSkyblockMod;
+import net.minecraft.util.ResourceLocation;
+
+public class GuiTextures {
+    public static final ResourceLocation SQUARE = new ResourceLocation(SplokysSkyblockMod.MODID, "textures/gui/square.png");
+}
