5f1aa0a8a2bfa6d6f91853988f0349ae7ea06d14
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-05-25 02:10:58 -0400

    Added ElementButton

---
+++ src/main/java/me/sploky/ssm/gui/buttons/ElementButton.java
@@ -0,0 +1,60 @@
+package me.sploky.ssm.gui.buttons;
+
+import me.sploky.ssm.elements.Element;
+import me.sploky.ssm.gui.Center;
+import me.sploky.ssm.gui.RenderUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+
+import java.awt.*;
+
+public class ElementButton extends GuiButton {
+    public static final int CORNER_BUTTON_SIZE = 6;
+
+    public Element element;
+
+    protected int corner;
+
+    public ElementButton(Element element, int corner) {
+        super(0, 0, 0, CORNER_BUTTON_SIZE, CORNER_BUTTON_SIZE, "");
+        this.element = element;
+        this.corner = corner;
+    }
+
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        switch (corner) {
+            case 0:
+                xPosition = element.getScreenPositionX() - element.sizeX / 2;
+                yPosition = element.getScreenPositionY() - element.sizeY / 2;
+                break;
+
+            case 1:
+                xPosition = element.getScreenPositionX() + element.sizeX / 2;
+                yPosition = element.getScreenPositionY() - element.sizeY / 2;
+                break;
+
+            case 2:
+                xPosition = element.getScreenPositionX() - element.sizeX / 2;
+                yPosition = element.getScreenPositionY() + element.sizeY / 2;
+                break;
+
+            case 3:
+                xPosition = element.getScreenPositionX() + element.sizeX / 2;
+                yPosition = element.getScreenPositionY() + element.sizeY / 2;
+                break;
+        }
+
+        xPosition -= width / 2;
+        yPosition -= height / 2;
+
+        RenderUtils.drawQuad(xPosition, yPosition, CORNER_BUTTON_SIZE, CORNER_BUTTON_SIZE, Color.WHITE, Center.TOP_LEFT_CORNER);
+    }
+
+    @Override
+    public void mouseReleased(int mouseX, int mouseY) {
+        super.mouseReleased(mouseX, mouseY);
+    }
+
+
+}
