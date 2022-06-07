3edeb632c666baca1327481658279f0e7f68ef35
Themaster-123 <72712061+Themaster-123@users.noreply.github.com>
2022-06-07 15:16:39 -0400

    Added fetchData method and moved getData, updateData methods to HypixelUtils

--- src/main/java/me/sploky/ssm/hypixeldata/HypixelUtils.java
+++ src/main/java/me/sploky/ssm/hypixeldata/HypixelUtils.java
@@ -1,5 +1,6 @@
 package me.sploky.ssm.hypixeldata;
 
+import me.sploky.ssm.SplokysSkyblockMod;
 import net.hypixel.api.HypixelAPI;
 import net.hypixel.api.apache.ApacheHttpClient;
 import net.hypixel.api.reply.skyblock.SkyBlockProfilesReply;
@@ -8,7 +9,7 @@ import net.minecraft.client.Minecraft;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class HypixelUtils {
+public final class HypixelUtils {
     private static UUID API_KEY = null;
     public static HypixelAPI API;
 
@@ -21,12 +22,31 @@ public class HypixelUtils {
     public static void setApiKey(UUID apiKey) {
         API_KEY = apiKey;
         API = new HypixelAPI(new ApacheHttpClient(apiKey));
+        fetchData();
+    }
+
+    public static void fetchData() {
+        if (API == null) return;
 
         try {
             SKYBLOCK_PROFILES = HypixelUtils.API.getSkyBlockProfiles(Minecraft.getMinecraft().thePlayer.getUniqueID()).get();
 
         } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
+        }
+
+        getData();
+    }
+
+    public static void getData() {
+        for (HypixelData data : SplokysSkyblockMod.main.hypixelDataSet) {
+            data.GetData();
+        }
+    }
+
+    public static void updateData() {
+        for (HypixelData data : SplokysSkyblockMod.main.hypixelDataSet) {
+            data.UpdateData();
         }
     }
 }
